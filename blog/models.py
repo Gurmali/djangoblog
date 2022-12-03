fbe9b7ac7f79a898a5e02d0c288339ab87b08bb3
Gurmali <Gurmali@hotmail.com>
2022-12-03 15:24:13 +0000

    Build admin site

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class post(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
@@ -30,9 +30,9 @@ class post(models.Model):
         return self.likes.count()
 
 
-class comment(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
